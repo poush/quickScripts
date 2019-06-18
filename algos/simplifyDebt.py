a23d6c6d90deb581979789512d3040cddb1591ae
Piyush Agrawal <poush12@gmail.com>
2019-06-18 18:35:21 +0530

    Update simplifyDebt.py

--- algos/simplifyDebt.py
+++ algos/simplifyDebt.py
@@ -1,4 +1,3 @@
-# Sample data
 transactions = [
     {   'paidBy': 'A',  'paidFor': { 'B': 100, 'C': 50 } },
     {   'paidBy': 'A',  'paidFor': { 'C': 500 } },
@@ -6,74 +5,69 @@ transactions = [
     {   'paidBy': 'C',  'paidFor': { 'A': 250, 'B': 200 }   }
 ]
 
-# transactions = [
-#   { 'paidBy': 'B' , 'paidFor': { 'A': 200 }},
-#   { 'paidBy': 'C' , 'paidFor': { 'B': 100 }},
-#   { 'paidBy': 'A' , 'paidFor': { 'C': 100 }},
-# ]
+transactions1 = [
+    {   'paidBy': 'A',  'paidFor': { 'C': 300, 'E': 35, 'F': 35 } },
+    {   'paidBy': 'B',  'paidFor': { 'D': 40, 'G': 10 } }
+]
 
-# transactions = [
-#     {   'paidBy': 'A',  'paidFor': { 'C': 300, 'E': 40, 'F': 30 } },
-#     {   'paidBy': 'B',  'paidFor': { 'D': 50 } }
-# ]
-
-def findSame(txs, value):
-    # print(txs)
-    n = 0
-    while n<len(txs):
-        if txs[n][1] == -value:
-            return n
-        n+=1
-    return -1
+transactions2 = [
+    {   'paidBy': 'A',  'paidFor': { 'C': 300, 'E': 40, 'F': 30 } },
+    {   'paidBy': 'B',  'paidFor': { 'D': 50 } }
+]
 
 
-def simplifyDebts(transactions):
-    owes = []
-    people_dept = {}
+def simplifyDept(transactions):
+    subgraphs = []
+    curr = -1
+    transactions = sorted(transactions, key=lambda k: k['paidBy'])
     for transaction in transactions:
+        if len(subgraphs) < 1 or transaction.get('paidBy') not in subgraphs[curr]:
+            curr+=1
+            subgraphs.append({})
+        
         paidBy = transaction.get('paidBy')
-        if paidBy not in people_dept:
-            people_dept[paidBy] = 0
 
-        transfers = transaction.get('paidFor');
+        if paidBy not in subgraphs[curr]:
+            subgraphs[curr][paidBy] = 0
 
+        transfers = transaction.get('paidFor')
         for person in transfers:
-            if person in people_dept:
-                people_dept[person] += transfers[person]
+            if person not in subgraphs[curr]:
+                subgraphs[curr][person] = transfers[person]
             else:
-                people_dept[person] = transfers[person]
+                subgraphs[curr][person] += transfers[person]
 
             # removing from the debt of person who paid
-            people_dept[paidBy] -= transfers[person]
-
-    people_dept = sorted(people_dept.items(), key=lambda k: k[1])
-    while True:
-        
-        # print(people_dept)
-        # Now calculating who will pay to whom
-        total = len(people_dept)
-        debit = people_dept[total-1]
-        credit = people_dept[0]
-        ccheck = findSame(people_dept, debit[1])
-        credit = people_dept[ccheck] if ccheck is not -1 else credit
-        c = ccheck if ccheck is not -1 else 0
+            subgraphs[curr][paidBy] -= transfers[person]
 
 
-        if credit[1] == 0 and debit[1]  == 0:
-            # sorted, there is nothing to do now
-            # Rupya Pohonch gaya
-            break
+    for people_dept in subgraphs:
+        people_dept = sorted(people_dept.items(), key=lambda k: k[1])
 
-        m = min([-credit[1], debit[1]])
-        # settling the transaction
-        people_dept[c] = (credit[0], credit[1] + m)
-        people_dept[total-1] = (debit[0], debit[1] - m)
-        # break
-        
-        print("Person " + debit[0] + " will pay " + str(m) + " to "+ credit[0])
-        people_dept = sorted(people_dept, key=lambda k: k[1])
-        # print(people_dept)
+        while True:
+            # print(people_dept)
+            # Now calculating who will pay to whom
+            total = len(people_dept)
+            debit = people_dept[total-1]
+            credit = people_dept[0]
+            # ccheck = findSame(people_dept, debit[1])
+            # credit = people_dept[ccheck] if ccheck is not -1 else credit
+            # c = ccheck if ccheck is not -1 else 0
 
 
-if __name__ == "__main__":
-    simplifyDebts(transactions)
+            if credit[1] == 0 and debit[1]  == 0:
+                # sorted, there is nothing to do now
+                # Rupya Pohonch gaya
+                break
+
+            m = min([-credit[1], debit[1]])
+            # settling the transaction
+            people_dept[0] = (credit[0], credit[1] + m)
+            people_dept[total-1] = (debit[0], debit[1] - m)
+            # break
+            
+            print("Person " + debit[0] + " will pay " + str(m) + " to "+ credit[0])
+            people_dept = sorted(people_dept, key=lambda k: k[1])
+
+
+simplifyDept(transactions2)
