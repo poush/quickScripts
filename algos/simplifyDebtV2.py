4e0a3209af052bcec0b642e6110be3a9e041d584
Piyush Agrawal <poush12@gmail.com>
2019-06-20 00:48:44 +0530

    Update simplifyDebtV2.py

--- algos/simplifyDebtV2.py
+++ algos/simplifyDebtV2.py
@@ -1,3 +1,6 @@
+# Set of transactions
+# i,j, A
+# i has to pay j the A amount
 transactions = [
     [0,1 ,150],
     [0,2 ,250],
@@ -9,73 +12,86 @@ transactions = [
 ts = transactions
 
 
+# This Algorithm is based on the concept of simplifying
+# transitive relations iteratively
+# In each iteration it looks for A->B->C relation and simplfies as:
+# 1. Is it completely transitive? then make it A->C
+# 2. If not, can chain be simplified by reducing the amount of cash flow?
+# 3. Cycles are also reduced through the same way for eg: A->B->A is resolved using the same
+#    transitive concept, making it A->A and then reducing the edges that starts and
+#    end with same node.
+# The step 2 breaks the chain as much as possible so that smallest cycle can be found
+# and reduced to zero
+def simplifyDebt(ts):
+    cut = True
+    reattempt = 0
 
-cut = True
-reattempt = 0
-while True:
-    cut = False
-    # ts = ts2
-    for i in range(0, len(ts)):
-        # print(i, ts[i])
-        if ts[i][0] == ts[i][1]:
-            ts = ts[:i] + ts[i+1:]
-            cut = True
-            break
-
-
-        if i>0:
-            if ts[0][0] == ts[i][0] and ts[0][1] == ts[i][1]:
-                ts[0][2] = ts[0][2] + ts[i][2]
+    while True:
+        cut = False
+        # ts = ts2
+        for i in range(0, len(ts)):
+            # print(i, ts[i])
+            if ts[i][0] == ts[i][1]:
                 ts = ts[:i] + ts[i+1:]
                 cut = True
                 break
 
-            if ts[i-1][1] == ts[i][0]:
-                # print(ts, 'sm', i)
-                if ts[i-1][2] >= ts[i][2]:
-                    if ts[i-1][2] == ts[i][2]:
+
+            if i>0:
+                if ts[0][0] == ts[i][0] and ts[0][1] == ts[i][1]:
+                    ts[0][2] = ts[0][2] + ts[i][2]
+                    ts = ts[:i] + ts[i+1:]
+                    cut = True
+                    break
+
+                if ts[i-1][1] == ts[i][0]:
+                    # print(ts, 'sm', i)
+                    if ts[i-1][2] >= ts[i][2]:
+                        if ts[i-1][2] == ts[i][2]:
+                            ts[i-1][1] = ts[i][1]
+                            ts = ts[:i] + ts[i+1:]
+                        else:
+                            ts[i-1][2] = ts[i-1][2] - ts[i][2];
+                            ts[i][0] = ts[i-1][0]
+                    else:
+                        ts[i][2] = ts[i][2] - ts[i-1][2];
                         ts[i-1][1] = ts[i][1]
-                        ts = ts[:i] + ts[i+1:]
-                    else:
-                        ts[i-1][2] = ts[i-1][2] - ts[i][2];
-                        ts[i][0] = ts[i-1][0]
-                else:
-                    ts[i][2] = ts[i][2] - ts[i-1][2];
-                    ts[i-1][1] = ts[i][1]
-                cut = True
-                # print('ms', ts, i)
-                reattempt = 0
-                break
+                    cut = True
+                    # print('ms', ts, i)
+                    reattempt = 0
+                    break
 
-            if ts[0][1] == ts[i][0]:
-                if ts[0][2] >= ts[i][2]:
-                    if ts[0][2] == ts[i][2]:
-                        # print('yes', i)
+                if ts[0][1] == ts[i][0]:
+                    if ts[0][2] >= ts[i][2]:
+                        if ts[0][2] == ts[i][2]:
+                            # print('yes', i)
+                            ts[0][1] = ts[i][1]
+                            ts = ts[:i] + ts[i+1:]
+                        else:
+                            ts[0][2] = ts[0][2] - ts[i][2]
+                            ts[i][0] = ts[0][0]
+                    else:
+                        ts[i][2] = ts[i][2] - ts[0][2]
                         ts[0][1] = ts[i][1]
-                        ts = ts[:i] + ts[i+1:]
-                    else:
-                        ts[0][2] = ts[0][2] - ts[i][2]
-                        ts[i][0] = ts[0][0]
-                else:
-                    ts[i][2] = ts[i][2] - ts[0][2]
-                    ts[0][1] = ts[i][1]
 
-                cut = True
-                reattempt = 0
-                # print('sd', ts)
-                break
+                    cut = True
+                    reattempt = 0
+                    # print('sd', ts)
+                    break
 
 
-            ts = [ts[-1]] + ts[0:-1]
-            # print("re", ts)
-            reattempt += 1
-    if len(ts) is 1:
-        break
+                ts = [ts[-1]] + ts[0:-1]
+                # print("re", ts)
+                reattempt += 1
+        if len(ts) is 1:
+            break
 
-    if cut is True or reattempt <= len(ts) +1:
-        continue
-    else:
-        # print('called')
-        break
+        if cut is True or reattempt <= len(ts) +1:
+            continue
+        else:
+            # print('called')
+            break
 
-print(ts)
+    return ts
+
+print(simplifyDebt(ts))
